1d8d587ed6497c0da36cf419063b289920b9e29d
Justin Leemans <justintentje64@gmail.com>
2024-07-08 21:16:21 +0200

    Added new years table and controller and updated new view

---
+++ app/Models/Year.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+
+class Year extends Model
+{
+    use HasFactory;
+
+    protected function day(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Crypt::decryptString($value),
+            set: fn (string $value) => Crypt::encryptString($value),
+        );
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>Home</x-slot>
+    <div x-data="{ message: 'Hello, Alpine!' }" class="text-center mt-4">
+        <span x-text="message"></span>
+    </div>
+</x-layout>
